4431cf466a37b2a8124ee92573cb1426b1e32b08
ArpanaM <68733492+ArpanaM@users.noreply.github.com>
2024-05-29 23:54:29 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to CICD Jenkins with AWS Implementation'
+    return 'Welcome to CICD Jenkins with AWS Implementation -- Demo Project'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
