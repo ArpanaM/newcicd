4fb980b8095093862e568d8c420f6623e215ae6d
ArpanaM <68733492+ArpanaM@users.noreply.github.com>
2025-02-22 06:20:22 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to CICD Jenkins'
+    return 'Welcome to CICD Jenkins with email notification'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
